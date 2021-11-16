77a96f313423c43da02d5421db2c05ea5b53e83f
Simon <simongonand@gmail.com>
2021-11-16 10:41:30 +0100

    destroy a point remove it from the list

--- Assets/Editor/PathEditor.cs
+++ Assets/Editor/PathEditor.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 public class PathEditor : Editor
 {
     SerializedProperty waypointsList;
-
+    Path pathScript;
+        
     private void OnEnable()
     {
         waypointsList = serializedObject.FindProperty("waypoints");
+        pathScript = target as Path;
     }
 
     public override void OnInspectorGUI()
@@ -24,9 +26,13 @@ public class PathEditor : Editor
             GameObject obj = new GameObject("Waypoint " + waypointsList.arraySize);
             obj.transform.position = Vector3.zero;
             obj.transform.rotation = Quaternion.identity;
-            obj.AddComponent<Waypoint>();
+            obj.transform.SetParent(pathScript.transform);
+            Waypoint wp = obj.AddComponent<Waypoint>();
+            wp.parentPath = pathScript;
             // Set Waypoint value
-            waypointsList.InsertArrayElementAtIndex(waypointsList.arraySize );
+            waypointsList.InsertArrayElementAtIndex(waypointsList.arraySize);
+            SerializedProperty sp = waypointsList.GetArrayElementAtIndex(waypointsList.arraySize - 1);
+            sp.objectReferenceValue = wp;
             EditorUtility.SetDirty(obj);
             Undo.RegisterCreatedObjectUndo(obj, "Create waypoint");
         }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Waypoint : MonoBehaviour
 {
+    public Path parentPath;
+
+    private void OnDestroy()
+    {
+        parentPath.waypoints.Remove(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
