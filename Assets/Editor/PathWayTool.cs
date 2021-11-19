cf9049e989b4368c3ac27ae415cba2e037b2ad98
Simon <simongonand@gmail.com>
2021-11-19 13:27:47 +0100

    remove useless window

--- Assets/Editor/PathWayTool.cs
+++ Assets/Editor/PathWayTool.cs
@@ -3,29 +3,16 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class PathWayTool : EditorWindow
+public class PathWayTool
 {
-    [MenuItem("Tools/Home Tool/Path Way Tool #P")]
-    public static void ShowWindow()
+    [MenuItem("Tools/Home Tool/Create New Path #P")]
+    public static void CreatePath()
     {
-        GetWindow(typeof(PathWayTool));
-    }
-
-    private void OnGUI()
-    {
-        if (GUILayout.Button("Create Path"))
-        {
-            if (Object.FindObjectOfType(typeof(Path)))
-            {
-                Debug.Log("There's already a path in the scene");
-                return;
-            }
-            GameObject obj = new GameObject("Path");
-            obj.AddComponent<Path>();
-            obj.transform.position = Vector3.zero;
-            obj.transform.rotation = Quaternion.identity;
-            EditorUtility.SetDirty(obj);
-            Undo.RegisterCreatedObjectUndo(obj, "Create path");
-        }
+        GameObject obj = new GameObject("Path");
+        obj.AddComponent<Path>();
+        obj.transform.position = Vector3.zero;
+        obj.transform.rotation = Quaternion.identity;
+        EditorUtility.SetDirty(obj);
+        Undo.RegisterCreatedObjectUndo(obj, "Create path");
     }
 }
